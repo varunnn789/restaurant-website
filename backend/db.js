3b2e54fed1bf9c4e443bbd8e003aa4dc77dea7d5
varunnn789 <singhvarunnn780@gmail.com>
2025-01-02 18:08:26 -0500

    Backend Json updated

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    connectionString: 'postgresql://restaurant_db_aozf_user:do72GnfFKRnyPkUp2N4VlGHfrPKkCwQq@dpg-ctrgba1u0jms73fv2320-a.oregon-postgres.render.com/restaurant_db_aozf',
     ssl: {
         rejectUnauthorized: false
     }
